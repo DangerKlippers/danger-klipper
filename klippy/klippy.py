a997aa54031b32bf55044ab0f40242edb82884aa
Brandon <bwnance@gmail.com>
2023-12-05 12:39:20 -0500

    fix importlib (#100)

--- klippy/klippy.py
+++ klippy/klippy.py
@@ -4,7 +4,7 @@
 # Copyright (C) 2016-2020  Kevin O'Connor <kevin@koconnor.net>
 #
 # This file may be distributed under the terms of the GNU GPLv3 license.
-import sys, os, gc, optparse, logging, time, collections, importlib
+import sys, os, gc, optparse, logging, time, collections, importlib, importlib.util
 import util, reactor, queuelogger, msgproto
 import gcode, configfile, pins, mcu, toolhead, webhooks
 
