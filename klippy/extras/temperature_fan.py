6ce697a32c175ab664dd69a84e9eb67a5fa14756
Niels van Zwieten <81711+nielsvz@users.noreply.github.com>
2024-02-14 03:03:12 +0100

    temperature_fan: fix fan never turning off when not reversed (#152)

--- klippy/extras/temperature_fan.py
+++ klippy/extras/temperature_fan.py
@@ -151,7 +151,7 @@ class ControlBangBang:
         ):
             self.heating = self.reverse
         elif (
-            not self.heating == self.reverse
+            self.heating == self.reverse
             and temp <= target_temp - self.max_delta
         ):
             self.heating = not self.reverse
