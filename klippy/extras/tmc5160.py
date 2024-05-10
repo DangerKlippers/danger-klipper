7f793c6f8f67149bd5c5b5b0933bfe2c7875d938
Rogerio Goncalves <rogerlz@gmail.com>
2024-05-10 19:49:22 +0100

    Revert "bugfix: restore run_current after sensorless homing (#233)" (#235)
    This reverts commit a47811645e1ead5317c00c9f7bd38ae4307ea78e.

--- klippy/extras/tmc5160.py
+++ klippy/extras/tmc5160.py
@@ -283,9 +283,6 @@ class TMC5160CurrentHelper:
     def needs_home_current_change(self):
         return self._home_current != self.run_current
 
-    def needs_run_current_change(self):
-        return self._prev_current != self.run_current
-
     def set_home_current(self, new_home_current):
         self._home_current = min(MAX_CURRENT, new_home_current)
 
@@ -341,7 +338,7 @@ class TMC5160CurrentHelper:
 
     def set_current(self, run_current, hold_current, print_time, force=False):
         if (
-            run_current == self.run_current
+            run_current == self._prev_current
             and hold_current == self.req_hold_current
             and not force
         ):
