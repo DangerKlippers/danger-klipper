c2930696d0ee31744f3214f82016d915c3b9fa27
Dmitry Butyugin <dmbutyugin@google.com>
2024-05-19 22:22:03 +0100

    input_shaper: Added support of smooth input shapers
    Signed-off-by: Dmitry Butyugin <dmbutyugin@google.com>

--- klippy/kinematics/extruder.py
+++ klippy/kinematics/extruder.py
@@ -7,6 +7,53 @@ import math, logging
 import stepper, chelper
 
 
+class ExtruderSmoother:
+    def __init__(self, config, pa_model):
+        self.smooth_time = config.getfloat(
+            "pressure_advance_smooth_time", 0.040, above=0.0, maxval=0.200
+        )
+        # A 4-th order smoothing function that goes to 0 together with
+        # its derivative at the ends of the smoothing interval
+        self.a = [15.0 / 8.0, 0.0, -15.0, 0.0, 30.0]
+        self.pa_model = pa_model
+        self.axes = ["x", "y", "z"]
+
+    def update(self, gcmd):
+        self.smooth_time = gcmd.get_float("SMOOTH_TIME", self.smooth_time)
+
+    def update_pa_model(self, pa_model):
+        self.pa_model = pa_model
+
+    def enable_axis(self, axis):
+        if axis not in self.axes:
+            self.axes.append(axis)
+
+    def disable_axis(self, axis):
+        if axis in self.axes:
+            self.axes.remove(axis)
+
+    def update_extruder_kinematics(self, extruder_sk):
+        ffi_main, ffi_lib = chelper.get_ffi()
+        n = len(self.a)
+        success = True
+        smooth_time = self.smooth_time if self.pa_model.enabled() else 0.0
+        for axis in self.axes:
+            if (
+                not ffi_lib.extruder_set_smoothing_params(
+                    extruder_sk, axis.encode(), n, self.a, smooth_time
+                )
+                == 0
+            ):
+                success = False
+        return success
+
+    def get_status(self, eventtime):
+        return {"smooth_time": self.smooth_time}
+
+    def get_msg(self):
+        return "pressure_advance_smooth_time: %.6f" % (self.smooth_time,)
+
+
 class PALinearModel:
     name = "linear"
 
@@ -148,9 +195,7 @@ class ExtruderStepper:
         self.pa_model = config.getchoice(
             "pressure_advance_model", self.pa_models, PALinearModel.name
         )(config)
-        self.pressure_advance_smooth_time = config.getfloat(
-            "pressure_advance_smooth_time", 0.040, above=0.0, maxval=0.200
-        )
+        self.smoother = ExtruderSmoother(config, self.pa_model)
         self.pressure_advance_time_offset = config.getfloat(
             "pressure_advance_time_offset", 0.0, minval=-0.2, maxval=0.2
         )
@@ -202,22 +247,21 @@ class ExtruderStepper:
         )
 
     def _handle_connect(self):
-        self.toolhead = self.printer.lookup_object("toolhead")
-        self.toolhead.register_step_generator(self.stepper.generate_steps)
+        toolhead = self.printer.lookup_object("toolhead")
+        toolhead.register_step_generator(self.stepper.generate_steps)
         self._update_pressure_advance(
-            self.pa_model,
-            self.pressure_advance_smooth_time,
-            self.pressure_advance_time_offset,
+            self.pa_model, self.pressure_advance_time_offset
         )
+        self.smoother.update_extruder_kinematics(self.sk_extruder)
 
     def get_status(self, eventtime):
         sts = {
             "pressure_advance_model": self.pa_model.name,
-            "smooth_time": self.pressure_advance_smooth_time,
             "time_offset": self.pressure_advance_time_offset,
             "motion_queue": self.motion_queue,
         }
         sts.update(self.pa_model.get_status(eventtime))
+        sts.update(self.smoother.get_status(eventtime))
         return sts
 
     def find_past_position(self, print_time):
@@ -225,7 +269,8 @@ class ExtruderStepper:
         return self.stepper.mcu_to_commanded_position(mcu_pos)
 
     def sync_to_extruder(self, extruder_name):
-        self.toolhead.flush_step_generation()
+        toolhead = self.printer.lookup_object("toolhead")
+        toolhead.flush_step_generation()
         if not extruder_name:
             if self.extruder is not None:
                 self.extruder.unlink_extruder_stepper(self)
@@ -241,8 +286,9 @@ class ExtruderStepper:
         self.motion_queue = extruder_name
         self.extruder = extruder
 
-    def _update_pressure_advance(self, pa_model, smooth_time, time_offset):
-        self.toolhead.flush_step_generation()
+    def _update_pressure_advance(self, pa_model, time_offset):
+        toolhead = self.printer.lookup_object("toolhead")
+        toolhead.flush_step_generation()
         ffi_main, ffi_lib = chelper.get_ffi()
         old_delay = ffi_lib.extruder_get_step_gen_window(self.sk_extruder)
         if self.pa_model.name != pa_model.name:
@@ -252,29 +298,34 @@ class ExtruderStepper:
             )
         pa_params = pa_model.get_pa_params()
         ffi_lib.extruder_set_pressure_advance(
-            self.sk_extruder,
-            len(pa_params),
-            pa_params,
-            smooth_time,
-            time_offset,
+            self.sk_extruder, len(pa_params), pa_params, time_offset
         )
+        self.smoother.update_pa_model(pa_model)
+        self.smoother.update_extruder_kinematics(self.sk_extruder)
         new_delay = ffi_lib.extruder_get_step_gen_window(self.sk_extruder)
         if old_delay != new_delay:
-            self.toolhead.note_step_generation_scan_time(new_delay, old_delay)
+            toolhead.note_step_generation_scan_time(new_delay, old_delay)
         self.pa_model = pa_model
-        self.pressure_advance_smooth_time = smooth_time
         self.pressure_advance_time_offset = time_offset
 
     def update_input_shaping(self, shapers):
         ffi_main, ffi_lib = chelper.get_ffi()
         old_delay = ffi_lib.extruder_get_step_gen_window(self.sk_extruder)
         failed_shapers = []
-        for shaper in self.shapers:
+        for shaper in shapers:
             if not shaper.update_extruder_kinematics(self.sk_extruder):
                 failed_shapers.append(shaper)
+            # Pressure advance requires extruder smoothing, make sure that
+            # some smoothing is enabled
+            if shaper.is_smoothing() and shaper.is_enabled():
+                self.smoother.disable_axis(shaper.get_axis())
+            elif not shaper.is_smoothing() or not shaper.is_enabled():
+                self.smoother.enable_axis(shaper.get_axis())
+        self.smoother.update_extruder_kinematics(self.sk_extruder)
         new_delay = ffi_lib.extruder_get_step_gen_window(self.sk_extruder)
+        toolhead = self.printer.lookup_object("toolhead")
         if old_delay != new_delay:
-            self.toolhead.note_step_generation_scan_time(new_delay, old_delay)
+            toolhead.note_step_generation_scan_time(new_delay, old_delay)
         return failed_shapers
 
     cmd_SET_PRESSURE_ADVANCE_help = "Set pressure advance parameters"
@@ -298,24 +349,20 @@ class ExtruderStepper:
         if pa_model_name != self.pa_model.name:
             pa_model = self.pa_models[pa_model_name]()
         pa_model.update(gcmd)
-        smooth_time = gcmd.get_float(
-            "SMOOTH_TIME",
-            self.pressure_advance_smooth_time,
-            minval=0.0,
-            maxval=0.200,
-        )
+        self.smoother.update(gcmd)
         time_offset = gcmd.get_float(
             "TIME_OFFSET",
             self.pressure_advance_time_offset,
             minval=-0.2,
             maxval=0.2,
         )
-        self._update_pressure_advance(pa_model, smooth_time, time_offset)
-        msg = "pressure_advance_model: %s\n" % (
-            pa_model.name,
-        ) + pa_model.get_msg() + "\npressure_advance_smooth_time: %.6f" "\npressure_advance_time_offset: %.6f" % (
-            smooth_time,
-            time_offset,
+        self._update_pressure_advance(pa_model, time_offset)
+        msg = (
+            "pressure_advance_model: %s\n" % (pa_model.name,)
+            + pa_model.get_msg()
+            + "\n"
+            + self.smoother.get_msg()
+            + "\npressure_advance_time_offset: %.6f" % (time_offset,)
         )
         self.printer.set_rollover_info(self.name, "%s: %s" % (self.name, msg))
         gcmd.respond_info(msg, log=False)
