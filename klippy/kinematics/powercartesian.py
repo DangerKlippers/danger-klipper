7b83c136854f2bb22cfc6e5e71341c9418c5d4dc
Bea Nance <bwnance@gmail.com>
2024-07-12 19:08:48 -0400

    powercartesian and fix powercorexy

--- klippy/kinematics/powercartesian.py
+++ klippy/kinematics/powercartesian.py
@@ -3,14 +3,14 @@
 # Copyright (C) 2017-2021  Kevin O'Connor <kevin@koconnor.net>
 #
 # This file may be distributed under the terms of the GNU GPLv3 license.
-from .corexy import CoreXYKinematics
+from .cartesian import CartKinematics
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
     from ..extras.powercore import PowerCore
 
 
-class PowerCoreXYKinematics(CoreXYKinematics):
+class PowerCartesianKinematics(CartKinematics):
     def __init__(self, toolhead, config):
         super().__init__(toolhead, config)
         self.powercore: PowerCore = config.get_printer().lookup_object(
@@ -23,4 +23,4 @@ class PowerCoreXYKinematics(CoreXYKinematics):
 
 
 def load_kinematics(toolhead, config):
-    return PowerCoreXYKinematics(toolhead, config)
+    return CartKinematics(toolhead, config)
